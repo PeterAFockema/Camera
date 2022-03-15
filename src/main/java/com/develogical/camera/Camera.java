d6bf790a9d53de18e664402f58e0c4a73cf3d23d
ape04 <test@test.de>
2022-03-15 16:22:05 +0000

    Add shutter sensor and memory card test.

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -3,13 +3,22 @@ package com.develogical.camera;
 public class Camera {
 
     private final Sensor sensor;
+    private final MemoryCard memoryCard;
 
     public Camera(final Sensor sensor) {
         this.sensor = sensor;
+        this.memoryCard = null;
+    }
+
+    public Camera(final Sensor sensor, final MemoryCard memoryCard) {
+        this.sensor = sensor;
+        this.memoryCard = memoryCard;
     }
 
     public void pressShutter() {
-        // not implemented
+        if (this.memoryCard != null) {
+            this.memoryCard.write(this.sensor.readData(), null);
+        }
     }
 
     public void powerOn() {
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -2,19 +2,24 @@ package com.develogical.camera;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
 
+    public static final byte[] RETURN_VALUE = new byte[]{};
     private Sensor sensor;
     private Camera underTest;
+    private MemoryCard memoryCard;
+    private Camera underTestWithCard;
 
     @Before
     public void init() {
         this.sensor = mock(Sensor.class);
+        this.memoryCard = mock(MemoryCard.class);
         this.underTest = new Camera(sensor);
+        this.underTestWithCard = new Camera(this.sensor, this.memoryCard);
     }
 
     @Test
@@ -28,4 +33,21 @@ public class CameraTest {
         this.underTest.powerOff();
         verify(this.sensor).powerDown();
     }
+
+    @Test
+    public void pressingTheShutterWithPowerOnCopiesDataFromTheSensorToTheMemory() {
+        when(this.sensor.readData()).thenReturn(RETURN_VALUE);
+
+        this.underTestWithCard.powerOn();
+        this.underTestWithCard.pressShutter();
+        verify(this.memoryCard).write(Mockito.eq(RETURN_VALUE), Mockito.any());
+    }
+
+   // @Test
+    //public void pressingTheShutterWithPowerNotOnCopiesDataFromTheSensorToTheMemory() {
+    //    this.underTest.pressShutter();
+    //    byte[] returnValue = new byte[]{};
+    //   when(sensor.readData()).thenReturn(returnValue);
+   //     verifyNoMoreInteractions(this.memoryCard);
+    //}
 }
