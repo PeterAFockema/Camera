9e13ec1c5d600cf9aa910fa18a46eca6e1dbf03c
ape04 <test@test.de>
2022-03-15 15:46:05 +0000

    Power down sensor on camera power off. Test refactoring and tidying up.

--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,16 +1,31 @@
 package com.develogical.camera;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
+
+    private Sensor sensor;
+    private Camera underTest;
+
+    @Before
+    public void init() {
+        this.sensor = mock(Sensor.class);
+        this.underTest = new Camera(sensor);
+    }
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        Sensor sensor = mock(Sensor.class);
-        Camera underTest = new Camera(sensor);
-        underTest.powerOn();
-        verify(sensor).powerUp();
+        this.underTest.powerOn();
+        verify(this.sensor).powerUp();
+    }
+
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        this.underTest.powerOff();
+        verify(this.sensor).powerDown();
     }
 }
