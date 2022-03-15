6dd71e1dfa073c481548b95570655bf76da65891
ape04 <test@test.de>
2022-03-15 15:41:10 +0000

    Power up sensor on camera power up.

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,12 +2,18 @@ package com.develogical.camera;
 
 public class Camera {
 
+    private final Sensor sensor;
+
+    public Camera(final Sensor sensor) {
+        this.sensor = sensor;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        this.sensor.powerUp();
     }
 
     public void powerOff() {
